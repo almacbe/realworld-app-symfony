c7cceaa2dc72f7b350724ceadd67e8e00dbf18e5
Alfonso Machado <alfonsomachado@spotahome.com>
2021-09-25 09:48:28 +0200

    add user fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('user@moltboni.co');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword(
+            $this->passwordHasher->hashPassword(
+                $user,
+                'password'
+            )
 
+        );
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
